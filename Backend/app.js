06a913f64230e5fc6885048eafa761fd94d93a7b
Victor <64720348+Gitpiff@users.noreply.github.com>
2024-08-01 01:52:56 +0100

    auth me backend phase 0 done

--- Backend/app.js
+++ Backend/app.js
@@ -8,6 +8,8 @@ const cookieParser = require('cookie-parser');
 const { environment } = require('./config');
 const isProduction = environment === 'production';
 
+const routes = require('./routes');
+
 const app = express();
 
 app.use(morgan('dev'));
@@ -38,3 +40,7 @@ if (!isProduction) {
       }
     })
   );
+
+  app.use(routes); 
+
+  module.exports = app;
